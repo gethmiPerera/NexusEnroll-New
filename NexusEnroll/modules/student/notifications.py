b4bc585ff7d620016ff6c6c9029bab3bcf888d4f
gethmiPerera <2023cs141@stu.ucsc.cmb.ac.lk>
2025-09-03 11:05:29 +0530

    test

--- NexusEnroll/modules/student/notifications.py
+++ NexusEnroll/modules/student/notifications.py
@@ -5,3 +5,5 @@ def send_notification(student, message):
     Sends/logs a notification to the student.
     """
     print(f"[Notification] To: {student.name} ({student.student_id}) -> {message}")
+
+#test
